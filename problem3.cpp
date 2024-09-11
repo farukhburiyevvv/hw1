fa1be202a31495568105c7f17e31076b7141864e
farrukh <f.buriyev@newuu.uz>
2024-09-11 14:57:11 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     double a;
     cin>>a;
-    cout<<"The temprature in Farenheit is equal to "<<(a*1.8)+32<<endl;
+    cout<<"The temperature in Farenheit is equal to "<<(a*1.8)+32<<endl;
 
     return 0;
 }
